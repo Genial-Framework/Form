340f76558cf7c34091ab8b66e00d8c5e621c4ecb
Nicholas English <30304307+Nenglish7@users.noreply.github.com>
2018-04-03 16:24:22 -0400

    Create ConfigInterface.php

---
+++ src/ConfigInterface.php
@@ -0,0 +1,20 @@
+<?php
+declare(strict_types=1);
+/**
+ * Genial Framework.
+ *
+ * @author  Nicholas English <nenglish6657@gmail.com>.
+ * @link    <https://github.com/Genial-Framework/Form> Github Repository.
+ * @license <https://github.com/Genial-Framework/Form/blob/master/LICENSE> MIT License.
+ *
+ * @package genial-framework/form
+ */
+
+namespace Genial\Form;
+
+/**
+ * ConfigInterface.
+ */
+interface ConfigInterface extends ConfigurationInterface
+{
+}
