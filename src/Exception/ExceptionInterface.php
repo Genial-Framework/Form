ab316665b983fa88aa9581a614bb21f2dfd188bf
Nicholas English <30304307+Nenglish7@users.noreply.github.com>
2018-04-03 15:30:37 -0400

    Create ExceptionInterface.php

---
+++ src/Exception/ExceptionInterface.php
@@ -0,0 +1,22 @@
+<?php
+declare(strict_types=1);
+/**
+ * Genial Framework.
+ *
+ * @author  Nicholas English <nenglish6657@gmail.com>.
+ * @link    <https://github.com/Genial-Framework/Form> Github Repository.
+ * @license <https://github.com/Genial-Framework/Form/blob/master/LICENSE> MIT License.
+ *
+ * @package genial-framework/form
+ */
+
+namespace Genial\Form\Exception;
+
+use Throwable;
+
+/**
+ * ExceptionInterface.
+ */
+interface ExceptionInterface extends Throwable
+{
+}
