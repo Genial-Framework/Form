8737b18e2fc17ae7056c5b6ac750b873d88911db
Nicholas English <30304307+Nenglish7@users.noreply.github.com>
2018-04-03 23:06:27 -0400

    Change docblock.

--- src/ConfigurationInterface.php
+++ src/ConfigurationInterface.php
@@ -3,11 +3,13 @@ declare(strict_types=1);
 /**
  * Genial Framework.
  *
- * @author  Nicholas English <nenglish6657@gmail.com>.
+ * @author  Nicholas English         <nenglish6657@gmail.com>.
+ * @author  Genial\Form Contributors <https://github.com/Genial-Framework/Form/graphs/contributors>.
+ *
  * @link    <https://github.com/Genial-Framework/Form> Github Repository.
  * @license <https://github.com/Genial-Framework/Form/blob/master/LICENSE> MIT License.
  *
- * @package genial-framework/form
+ * @package genial-framework/form.
  */
 
 namespace Genial\Form;
